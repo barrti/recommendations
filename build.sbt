d10303046b5851a02ae1687b57843da37414b43f
fcygan <fcygan@pgs-soft.com>
2017-10-16 13:12:12 +0200

    Add job for recommendations

--- build.sbt
+++ build.sbt
@@ -4,6 +4,12 @@ version := "1.0"
 
 scalaVersion := "2.11.9"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.1"
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.1.1"
-libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.1.1"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.2.0"
+
+resolvers += "Job Server Bintray" at "https://dl.bintray.com/spark-jobserver/maven"
+
+libraryDependencies += "spark.jobserver" %% "job-server-api" % "0.7.0" % "provided"
+
+libraryDependencies += "spark.jobserver" %% "job-server-extras" % "0.7.0" % "provided"
---
+++ src/main/scala/com/pgssoft/movies/RecommendationJob.scala
@@ -0,0 +1,53 @@
+package com.pgssoft.movies
+
+import com.typesafe.config.Config
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+import spark.jobserver.{SparkJobValid, SparkJobValidation, SparkSqlJob}
+
+object RecommendationJob extends SparkSqlJob {
+
+  def validate(sql: SQLContext, config: Config): SparkJobValidation = SparkJobValid
+
+  def runJob(sql: SQLContext, config: Config): Any = {
+
+    val myRating = sql.read.jdbc(null, null, null) // load myRating from database
+
+
+    val model = MatrixFactorizationModel.load(sql.sparkContext, "")
+    // read trained model from parquet file
+    val candidates = sql.read.jdbc(null, null, null) // loading candidates
+    val movies = sql.read.jdbc(null, null, null) //loading movies
+    val rdd: RDD[(Int, Int)] = candidates.map((1000, _))
+
+    val recommendations = model.predict(rdd)
+      .collect()
+      .sortBy(-_.rating)
+      .take(50)
+
+    var i = 1
+    println("Movies recommended for you:")
+    recommendations.foreach { r =>
+      println("%2d".format(i) + ": " + movies(r.product) + " rating: " + r.rating)
+      i += 1
+    }
+  }
+
+  def main(args: Array[String]) {
+
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
+
+    // set up environment
+
+    val conf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("MovieLensALS")
+      .set("spark.executor.memory", "2g")
+    val sc = new SparkContext(conf)
+  }
+
+}
